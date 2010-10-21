663ed00aa24618696e678ffe5cf00d2475526576
Sike Huang <Sike Huang@SikeHuang-PC>
2010-10-22 00:00:28 +0200

    sqlalchemy

---
+++ project/python/src/gmail.py
@@ -0,0 +1,18 @@
+import smtplib
+
+fromaddr = 'sike.huang@gmail.com'
+toaddrs = 'sike.huang@live.com'
+
+msg = 'hello world'
+
+username = 'sike.huang@gmail.com'
+password = '54Scott850414'
+
+server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+server.login(username, password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
+
